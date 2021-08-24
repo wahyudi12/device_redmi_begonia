39fbff387694029d6172f923e9632896a43891f0
TeraaBytee <terabyte3766@gmail.com>
2021-08-24 15:40:24 +0700

    begonia: create specific nad overlay Signed-off-by: TeraaBytee <terabyte3766@gmail.com>

--- device.mk
+++ device.mk
@@ -132,7 +132,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-nad
 
 # Permissions
 PRODUCT_COPY_FILES += \
