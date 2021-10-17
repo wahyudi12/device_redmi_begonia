PixelExperience-Devices/device_xiaomi_begonia

begonia: Enforce vendor trees includes presence
A compiled build has no chance of booting without the prebuilt
binaries, so fail the build if those are not present.

Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
Change-Id: Idbf97b91e9aa6afc2b6a9f6af2750ae7c67ce852

BoardConfig.mk
@@ -118,4 +118,4 @@ BOARD_VNDK_RUNTIME_DISABLE := true
 PRODUCT_VENDOR_MOVE_ENABLED := true
 
 # Inherit from the proprietary version
--include vendor/xiaomi/begonia/BoardConfigVendor.mk
+include vendor/xiaomi/begonia/BoardConfigVendor.mk

wahyudi12/device_redmi_begonia

BoardConfig.mk
@@ -153,4 +153,4 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Inherit from the proprietary version
--include vendor/redmi/begonia/BoardConfigVendor.mk
+include vendor/redmi/begonia/BoardConfigVendor.mk